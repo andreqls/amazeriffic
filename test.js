3d877f20204ea2f97645f9fedc3d7f46212ac2d7
Andre Silva <andreqls@gmail.com>
2020-02-17 20:12:32 -0300

    Tested functions successfully

--- test.js
+++ test.js
@@ -25,16 +25,70 @@ var toDoObjects = [
 	}
 ];
 
-var organizeByTags = function (toDoObjects) {
-	console.log("Organize by tabs called");
-	return toDoObjects;
+/*	TAGS:
+ *	[
+ *		{
+ *			"name": "shopping",
+ *			"toDos": ["Get groceries", "Buy cough syrup"]
+ *		},
+ *
+ *		{
+ *			"name": "chores",
+ *			"toDos": ["Bench-press the truck", "Find needle in haystack"]
+ *		}
+ *	]
+ *
+ *	TODOS:
+ *	[
+ *		{
+ *			"description": "Answer emails",
+ *			"tags": ["work", ...]
+ *		}, ...
+ *	]
+ * */
+
+var tagIndex = function (tag, tagsList) {
+	var i;
+	for (i=0;i<tagsList.length;i++) {
+		if (tagsList[i].name.localeCompare(tag)==0) {
+			console.log("TRUE: tagElement.name="+tagsList[i].name+", tag="+tag);
+			return i;
+		}
+	}
+	return -1;
+};
+
+var updateTag = function (tagArray, index, newTask) {
+	tagArray[index].toDos.push(newTask);
+};
+
+var organizeByTags = function (toDosList) {
+	var organizedByTags = [];
+	toDosList.forEach(function (toDoItem) { // toDoItem = { "description":"...", "tags":[...] }
+		toDoItem.tags.forEach(function (tagItem) { // tagItem="..."
+			var ind=tagIndex(tagItem, organizedByTags);
+			if (ind==-1) {
+				organizedByTags.push({
+					"name": tagItem,
+					"toDos": [toDoItem.description]
+				});
+			}
+			else updateTag(organizedByTags, ind, toDoItem.description);
+		});
+	});
+	return organizedByTags;
+};
+
+var testReturn = function (parameter) {
+	if (parameter==0) return true;
+	return false;
 };
 
 var main = function () {
 	"use strict";
-
-	console.log(organizeByTags(toDoObjects));
-
+	var organizedByTags=organizeByTags(toDoObjects);
+	console.log(organizedByTags);
+	console.log(testReturn(0));
 };
 
 $(document).ready(main);

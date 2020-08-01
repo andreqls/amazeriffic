99ba8ff7ba2319fc7dea05ad49f44b80d57301c0
Andre Silva <andreqls@gmail.com>
2020-08-01 09:14:16 -0300

    Added interactivity

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,15 @@
 var main = function () {
     "use strict";
 
-    console.log("Hello World!");
+    $(".tabs div").toArray().forEach(function (element) {
+        $(element).on("click", function () {
+            $(".tabs div").removeClass("active");
+            $(element).addClass("active");
+            $("main .content").empty();
+            return false; // so that the browser doesn't try to follow the link
+        });
+    });
+
 };
 
 $(document).ready(main);

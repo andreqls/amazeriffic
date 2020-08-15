f00ef78ac61b72d7d275bf4499f5a813d20447e3
Andre Silva <andreqls@gmail.com>
2020-08-15 12:29:18 -0300

    Persistence acheived with MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express=require("express"),
 	http=require("http"),
+    mongoose=require("mongoose"),
 	app=express(),
 	toDos=[
 		{
@@ -15,17 +16,55 @@ var express=require("express"),
 app.use(express.static(__dirname+"/client"));
 app.use(express.urlencoded());
 
+/*-----MongoDB setup-----*/
+
+mongoose.connect('mongodb://localhost/amazeriffic');
+
+var ToDoSchema = mongoose.Schema({
+    description: String,
+    tags: [String]
+});
+
+var ToDo = mongoose.model("ToDo", ToDoSchema);
+
+/*-----*/ 
+
 http.createServer(app).listen(3000);
 
 app.get("/todos.json",function(req,res){
-	res.json(toDos);
+    ToDo.find({}, function (err, toDos) {
+        if (err!=null) console.log("ERROR: "+err);
+        else res.json(toDos);
+    });
 });
 
+app.post("/todos", function(req,res){
+    console.log(req.body);
+    
+    var newToDo = new ToDo({
+        "description":req.body.description,
+        "tags":req.body.tags
+    });
+    
+    newToDo.save(function (err, result) {
+        if (err!=null) {
+            console.log(err);
+            res.send("ERROR: "+err);
+        }
+        else {
+            ToDo.find({}, function (err, result) {
+                if (err!=null) res.send("ERROR: "+err);
+                else res.json(result);
+            });
+        }
+    });
+});
 
+/*
 app.post("/todos", function (req,res) {
     var newToDo = req.body;
     console.log(newToDo);
     toDos.push(newToDo);
     res.json({"message":"You posted to the server!"});
-});
+});*/
 
